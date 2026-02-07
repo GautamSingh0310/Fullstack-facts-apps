81ef08d3c9857afa7367979b63f0f237e4967b0d
GautamSingh0310 <singhgautam0304@gmail.com>
2026-02-08 00:34:27 +0530

    Update frontend API URL

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ function App() {
   const [facts, setFacts] = useState([]);
 
   useEffect(() => {
-    fetch("http://127.0.0.1:8000/api/facts/")
+    fetch("https://fullstack-facts-apps.onrender.com")
       .then((res) => res.json())
       .then((data) => setFacts(data))
       .catch((err) => console.error("Error fetching facts:", err));
